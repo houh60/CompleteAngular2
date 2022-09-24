c461cb28146ce759ad6b1a9eea4b4d1c617f0cc0
William Hou <williamhou38@yahoo.com>
2022-09-24 05:38:10 -0500

    new changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,7 @@ export class RecipeListComponent implements OnInit {
     recipes: Recipe[] = [
         new Recipe('1st Test Recipe', 'This is the first test recipe.', 'https://www.eatwell101.com/wp-content/uploads/2020/10/Garlic-Butter-Steak-recipe-roasted-in-Oven.jpg'),
         new Recipe('2nd Test Recipe', 'This is the second test recipe.', 'https://assets.epicurious.com/photos/563b9cdf4cc2d53b6f5144b6/master/w_1280,c_limit/388552_porterhouse-steak_1x1.jpg'),
-        new Recipe('3rd Test Recipe', 'This is the third test recipe.', 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2017_38/1284135/al-rokers-grilled-rib-eye-today-tease-170922.jpg'),
+        new Recipe('3rd Test Recipe', 'This is the third test recipe.', 'https://assets.epicurious.com/photos/563b9cdf4cc2d53b6f5144b6/master/w_1280,c_limit/388552_porterhouse-steak_1x1.jpg'),
     ];
     constructor() {}
 
