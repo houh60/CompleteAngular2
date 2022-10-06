815471271d2fe2bc4a9d4a45552c5352bcda96f0
William Hou <williamhou38@yahoo.com>
2022-10-06 07:59:35 -0500

    first commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+    @Output() featureSelected = new EventEmitter();
+
+    onSelected(feature: string) {
+        if(feature == 'recipes') {
+            this.featureSelected.emit('recipes');
+        } else {
+            this.featureSelected.emit('shopping-list');
+        }
+    }
+}
